616be31c50fef982454ecdac64314ba72079e023
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-17 19:58:57 +0200

    Add main.adb with entry point and messages

---
+++ main.adb
@@ -0,0 +1,9 @@
+-- main.adb
+-- Entry point for generic execution (Compilation requirement)
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line("Halftoning Project compiled successfully.");
+   Put_Line("Run `make test` to execute the V&V test suite.");
+end Main;
